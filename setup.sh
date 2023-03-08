637731e817cb24cf09f706de26ecd51172951893
kuku <61699377+kuku744@users.noreply.github.com>
2023-03-08 21:47:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-brew install --cask raycast karabiner-elements rectangle iterm2 neovim
+brew install --cask raycast karabiner-elements rectangle iterm2
+brew install neovim
 brew tap homebrew/cask-fonts
 brew install font-fira-code-nerd-font
